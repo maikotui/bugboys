9b2b24cc16332d28e95cb4f68788b18c0c7f4da6
maikotui <maikotuitupou@protonmail.com>
2025-03-24 20:45:04 -0600

    [Init] Base project from https://fabricmc.net/develop/template/

---
+++ src/client/java/dev/maikotui/bugboys/BugBoysMCDataGenerator.java
@@ -0,0 +1,11 @@
+package dev.maikotui.bugboys;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class BugBoysMCDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
